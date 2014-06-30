23b6665bf6525d479a9bb7c9a76c6a361fa6a9a1
Russell King <rmk+kernel@arm.linux.org.uk>
2014-06-30 16:29:12 +0100

    ARM: convert all "mov.* pc, reg" to "bx reg" for ARMv6+
    ARMv6 and greater introduced a new instruction ("bx") which can be used
    to return from function calls.  Recent CPUs perform better when the
    "bx lr" instruction is used rather than the "mov pc, lr" instruction,
    and this sequence is strongly recommended to be used by the ARM
    architecture manual (section A.4.1.1).
    
    We provide a new macro "ret" with all its variants for the condition
    code which will resolve to the appropriate instruction.
    
    Rather than doing this piecemeal, and miss some instances, change all
    the "mov pc" instances to use the new macro, with the exception of
    the "movs" instruction and the kprobes code.  This allows us to detect
    the "mov pc, lr" case and fix it up - and also gives us the possibility
    of deploying this for other registers depending on the CPU selection.
    
    Reported-by: Will Deacon <will.deacon@arm.com>
    Tested-by: Stephen Warren <swarren@nvidia.com> # Tegra Jetson TK1
    Tested-by: Robert Jarzmik <robert.jarzmik@free.fr> # mioa701_bootresume.S
    Tested-by: Andrew Lunn <andrew@lunn.ch> # Kirkwood
    Tested-by: Shawn Guo <shawn.guo@freescale.com>
    Tested-by: Tony Lindgren <tony@atomide.com> # OMAPs
    Tested-by: Gregory CLEMENT <gregory.clement@free-electrons.com> # Armada XP, 375, 385
    Acked-by: Sekhar Nori <nsekhar@ti.com> # DaVinci
    Acked-by: Christoffer Dall <christoffer.dall@linaro.org> # kvm/hyp
    Acked-by: Haojian Zhuang <haojian.zhuang@gmail.com> # PXA3xx
    Acked-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com> # Xen
    Tested-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de> # ARMv7M
    Tested-by: Simon Horman <horms+renesas@verge.net.au> # Shmobile
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off by: Sultan Qasim Khan <sultanqasim@gmail.com>

--- arch/arm/lib/bitops.h
+++ arch/arm/lib/bitops.h
@@ -1,3 +1,4 @@
+#include <asm/assembler.h>
 #include <asm/unwind.h>
 
 #if __LINUX_ARM_ARCH__ >= 6
@@ -88,7 +89,7 @@ UNWIND(	.fnstart	)
 	\instr	r2, r2, r3
 	str	r2, [r1, r0, lsl #2]
 	restore_irqs ip
-	mov	pc, lr
+	ret	lr
 UNWIND(	.fnend		)
 ENDPROC(\name		)
 	.endm
@@ -116,7 +117,7 @@ UNWIND(	.fnstart	)
 	str\cond	r2, [r1]
 	moveq	r0, #0
 	restore_irqs ip
-	mov	pc, lr
+	ret	lr
 UNWIND(	.fnend		)
 ENDPROC(\name		)
 	.endm
