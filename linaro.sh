b1d9d51241c9f9c061fabff1119e5318ca5f24fe
kondors1995 <normandija1945@gmail.com>
2017-02-03 13:29:01 +0200

    fix

--- linaro.sh
+++ linaro.sh
@@ -13,7 +13,7 @@ then
         then
             export ARCH=arm
             export CROSS_COMPILE=~/Android/Kernel/toolchains/arm-eabi-6.2.1-linaro/bin/arm-eabi-
-            make g620s_defconfig
+            make cm_hwY550_defconfig
             echo "Enter number of threads: "
             read input_variable
             echo "You entered: $input_variable"
@@ -39,7 +39,7 @@ then
 
             export ARCH=arm
             export CROSS_COMPILE=~/Android/Kernel/toolchains/arm-eabi-6.2.1-linaro/bin/arm-eabi-
-            make g620s_defconfig
+            make cm_hwY550_defconfig
             echo "Enter number of threads: "
             read input_variable
             echo "You entered: $input_variable"
