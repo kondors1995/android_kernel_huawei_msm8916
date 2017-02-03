add5a6267f62193761869e5534b337f38efcb1cc
kondors1995 <normandija1945@gmail.com>
2017-02-03 12:04:42 +0200

    ....

---
+++ gcc6.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+echo Do you want to clean?
+read REPLY1
+if [[ $REPLY1 =~ ^[Yy]$ ]]
+then
+
+    make clean && make mrproper
+    git reset --hard
+    echo Do you want to compile kernel?
+    read REPLY2
+
+        if [[ $REPLY2 =~ ^[Yy]$ ]]
+        then
+            export ARCH=arm
+            export CROSS_COMPILE=~/Android/Kernel/toolchains/arm-eabi-6.x-uber/bin/arm-eabi-
+            make g620s_defconfig
+            echo "Enter number of threads: "
+            read input_variable
+            echo "You entered: $input_variable"
+            make -j$input_variable
+            cp arch/arm/boot/zImage '/home/quagmire/Desktop/2/uber'
+            find . -name '*ko' -exec cp '{}' '/home/quagmire/Desktop/2/uber' \;
+
+        fi
+
+elif [[ $REPLY1 =~ ^[Nn]$ ]]
+then
+
+    echo Do you want to compile kernel now?
+    read REPLY3
+
+        if [[ $REPLY3 =~ ^[Yy]$ ]]
+        then
+
+            export ARCH=arm
+            export CROSS_COMPILE=~/Android/Kernel/toolchains/arm-eabi-6.x-uber/bin/arm-eabi-
+            make g620s_defconfig
+            echo "Enter number of threads: "
+            read input_variable
+            echo "You entered: $input_variable"
+            make -j$input_variable
+            cp arch/arm/boot/zImage '/home/quagmire/Desktop/2/uber'
+            find . -name '*ko' -exec cp '{}' '/home/quagmire/Desktop/2/uber' \;
+
+        fi
+fi
+read -rsp $'Press any key to continue...\n' -n 1 key
+# echo $key
---
+++ linaro.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+echo Do you want to clean?
+read REPLY1
+if [[ $REPLY1 =~ ^[Yy]$ ]]
+then
+
+    make clean && make mrproper
+    git reset --hard
+    echo Do you want to compile kernel?
+    read REPLY2
+
+        if [[ $REPLY2 =~ ^[Yy]$ ]]
+        then
+            export ARCH=arm
+            export CROSS_COMPILE=~/Android/Kernel/toolchains/arm-eabi-6.2.1-linaro/bin/arm-eabi-
+            make g620s_defconfig
+            echo "Enter number of threads: "
+            read input_variable
+            echo "You entered: $input_variable"
+            make -j$input_variable
+            cp arch/arm/boot/zImage '/home/nato/Android/Kernel/AnyKernel2-master'
+            find . -name '*ko' -exec cp '{}' '/home/nato/Android/Kernel/AnyKernel2-master' \;
+            cd ~/Android/Kernel/AnyKernel2-master
+            echo "Enter number of threads: "
+            read input_variable
+            echo "You entered: $input_variable"
+            zip -r9 $input_variable * -x README $input_variable
+
+        fi
+
+elif [[ $REPLY1 =~ ^[Nn]$ ]]
+then
+
+    echo Do you want to compile kernel now?
+    read REPLY3
+
+        if [[ $REPLY3 =~ ^[Yy]$ ]]
+        then
+
+            export ARCH=arm
+            export CROSS_COMPILE=~/Android/Kernel/toolchains/arm-eabi-6.2.1-linaro/bin/arm-eabi-
+            make g620s_defconfig
+            echo "Enter number of threads: "
+            read input_variable
+            echo "You entered: $input_variable"
+            make -j$input_variable
+            cp arch/arm/boot/zImage '/home/nato/Android/Kernel/AnyKernel2-master'
+            find . -name '*ko' -exec cp '{}' '/home/nato/Android/Kernel/AnyKernel2-master' \;
+            cd ~/Android/Kernel/AnyKernel2-master
+            echo "Enter number of threads: "
+            read input_variable
+            echo "You entered: $input_variable"
+            zip -r9 $input_variable * -x README $input_variable
+
+        fi
+fi
+read -rsp $'Press any key to continue...\n' -n 1 key
+# echo $key
